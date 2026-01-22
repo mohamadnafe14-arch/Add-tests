cb27efcc4f76ce6da67349259fe772ca4637606b
mohamed <mnafe44@gmail.com>
2026-01-22 15:41:46 +0200

    adding integration test

---
+++ test/integration_test.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:to_do_app/features/home/presentation/views/home_view.dart';
+import 'package:to_do_app/main.dart' as app;
+import 'package:to_do_app/features/home/presentation/view_models/providers/to_do_provider.dart';
+import 'package:to_do_app/features/home/data/repo/home_repo_impl.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:to_do_app/features/home/data/models/to_do_model.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  group('ToDo App Integration Test', () {
+    late Box<ToDoModel> toDoBox;
+    late ToDoProvider toDoProvider;
+    setUpAll(() async {
+      await Hive.initFlutter();
+      Hive.registerAdapter(ToDoModelAdapter());
+      toDoBox = await Hive.openBox<ToDoModel>('integrationTestBox');
+      final homeRepo = HomeRepoImpl(toDoBox: toDoBox);
+      toDoProvider = ToDoProvider(homeRepo: homeRepo);
+    });
+    tearDownAll(() async {
+      await toDoBox.clear();
+      await toDoBox.close();
+      await Hive.deleteBoxFromDisk('integrationTestBox');
+    });
+    testWidgets('Add and Search Task', (tester) async {
+      await tester.pumpWidget(
+        ScreenUtilInit(
+          designSize: const Size(360, 690),
+          child: ChangeNotifierProvider.value(
+            value: toDoProvider,
+            child: MaterialApp(home: HomeView()),
+          ),
+        ),
+      );
+      await tester.pumpAndSettle();
+      final newTask = ToDoModel(
+        title: 'Integration Test Task',
+        createdAt: DateTime.now(),
+      );
+      toDoProvider.addToDo(newTask);
+      await tester.pumpAndSettle();
+      expect(find.text('Integration Test Task'), findsOneWidget);
+      final searchField = find.byType(TextField);
+      await tester.enterText(searchField, 'Integration Test Task');
+      await tester.pumpAndSettle();
+      expect(find.text('Integration Test Task'), findsOneWidget);
+    });
+  });
+}
